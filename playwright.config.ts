c56c5ca5ae103ef7ee9316891e71f557505d4f34
Meghana Punyavathi <meghana.mp.19@gmail.com>
2026-03-15 15:58:03 +0530

    perf: optimize workers and headless mode for Jenkins CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,18 +13,30 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['html'], 
+    ['junit', { outputFile: 'results/test-results.xml' }] // Jenkins uses this
+  ],
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+
+    headless: process.env.CI ? true : false,
+
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
